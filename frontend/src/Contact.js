c31f6c412540a82a2ab3fcf894630050bc3d8756
Dungdo <dungdo26091999@gmail.com>
2025-03-09 12:55:21 +0700

    css mobile

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <div className="contact-container"> {/* Thêm div này */}
       <h2>Liên hệ</h2>
-      <p>Tele: <a href="https://web.telegram.org/k/#@henrynguyen1021" target="_blank" rel="noopener noreferrer">https://web.telegram.org/k/#@henrynguyen1021</a></p>
+      <p>Tele: <a href="https://web.telegram.org/k/#@henrynguyen1021" target="_blank" rel="noopener noreferrer">@henrynguyen1021</a></p>
     </div>
   );
 }
